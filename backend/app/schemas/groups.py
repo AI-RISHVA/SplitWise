f103c8b8c411469774b35ffa4a1df9f87ce91474
AI-RISHVA <rishva9510@gmail.com>
2026-08-03 18:23:29 +0530

    added new pydantic class

--- backend/app/schemas/groups.py
+++ backend/app/schemas/groups.py
@@ -1,7 +1,20 @@
 from pydantic import BaseModel 
+from typing import Optional,List
 
-class GroupCreat(BaseModel):
+
+class GroupInfo(BaseModel):
     group_name : str
     group_description:str|None = None
     groupmember: list[str]
     #Field and JSON atle import kariya km k groupmember ma set che , and db ma set and list direct convert thy nahi atle a groupmember ne json ma covert kari
+
+    
+
+class UpdateGroup(BaseModel):
+    group_name: str  
+    new_group_name: Optional[str] = None  
+    group_description: Optional[str] = None  
+    groupmember: Optional[List[str]] = None  
+    
+    old_member: Optional[str] = None  
+    new_member: Optional[str] = None  
