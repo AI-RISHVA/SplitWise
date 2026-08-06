6626dd5e0e71aa145edf80bfde259fae6b46f1b5
AI-RISHVA <rishva9510@gmail.com>
2026-08-06 15:29:47 +0530

    connect new routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI,Depends
+from app.api.auth import router as auth_router   
 from app.api import users as users_routes
 from app.api import groups as groups_routes
 from app.api import expenses as expenses_routes
+from app.api.friend import router as friend_router
+from app.api import settlement as settlement_routes 
+from app.db.data import Base,engine
 
 
 from app.db import data
@@ -12,13 +16,16 @@ from contextlib import asynccontextmanager
 async def lifespan_manager(app:FastAPI): #lifespan ma lakhelo code only 1 time run thay avo hoy, jya e server start thy k stop thy 
     data.create_db_and_table()
     yield 
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="This is Splitwise", lifespan=lifespan_manager)
 
+app.include_router(auth_router)
 app.include_router(users_routes.router)
 app.include_router(groups_routes.router)
 app.include_router(expenses_routes.router)
-
+app.include_router(settlement_routes.router) 
+app.include_router(friend_router)
 
 @app.get("/")
 async def welcome():
