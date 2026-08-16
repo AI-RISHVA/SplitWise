7499ea9aeab6cbe514984dc8d3ef43036c4f7212
AI-RISHVA <rishva9510@gmail.com>
2026-08-16 21:09:42 +0530

    Connect Google login routes and setup rate limiting

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI,Depends
 from app.api.auth import router as auth_router   
-from app.api import users as users_routes
-from app.api import groups as groups_routes
-from app.api import expenses as expenses_routes
+from app.api.users import router as users_router
+from app.api.groups import router as groups_router
+from app.api.expenses import router as expenses_router
 from app.api.friend import router as friend_router
-from app.api import settlement as settlement_routes 
-from app.db.data import Base,engine
+from app.api.settlement import router as settlement_router
 
+from app.api.google import router as google_auth_router
+
+
+from app.db.data import Base,engine
+from fastapi.middleware.cors import CORSMiddleware
+from app.middleware.middleware import exception_middleware
+import os
+
+from starlette.middleware.sessions import SessionMiddleware
 
 from app.db import data
 from contextlib import asynccontextmanager
@@ -21,11 +29,27 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="This is Splitwise", lifespan=lifespan_manager)
 
+app.middleware("http")(exception_middleware)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],          #atyre am thik jyre deploy kariye tyre frontend ni url nakhvani (like "http://localhost:5173")
+    allow_credentials=True,
+    allow_methods=["*"],          # GET, POST, PUT, DELETE - allow
+    allow_headers=["*"]
+)
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=os.getenv("SECRET_KEY")
+)
+
+
 app.include_router(auth_router)
-app.include_router(users_routes.router)
-app.include_router(groups_routes.router)
-app.include_router(expenses_routes.router)
-app.include_router(settlement_routes.router) 
+app.include_router(users_router)
+app.include_router(google_auth_router)
+app.include_router(groups_router)
+app.include_router(expenses_router)
+app.include_router(settlement_router) 
 app.include_router(friend_router)
 
 @app.get("/")
